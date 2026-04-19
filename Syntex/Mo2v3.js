d4af97b9e12a91f90b9ee31eddb4fc2214099200
HKrobinvai <hkrobin49@gmail.com>
2026-04-19 23:35:48 +0600

    add the mo2vo3

---
+++ Syntex/Mo2v3.js
@@ -0,0 +1,32 @@
+const numbers = [40, 100, 1, 5, 25, 10];
+const fruits = ["Banana", "apple", "Cherry", "date"];
+
+fruits.sort((a,b)=> a.localeCompare(b))
+
+// console.log(fruits)
+
+// Nested array flat
+
+const arr = [1, 2, 3, [4, 5, [6, 7, [8, 9, [10, 11]]]]];
+
+const flatArray = arr.flat(Infinity)
+
+// console.log(flatArray)
+
+const tagsFromPosts = [
+  ["javascript", "react", "css"],
+  ["node", "express"],
+  ["css", "html", "react"],
+];
+
+
+const filterTags = [...new Set(tagsFromPosts.flat())];
+
+console.log(tagsFromPosts)
+
+
+
+
+
+
+
