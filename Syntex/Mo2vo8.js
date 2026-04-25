8c78d478de69334b9a9809a55b282f08d7f394eb
HKrobinvai <hkrobin49@gmail.com>
2026-04-25 22:06:42 +0600

    added the mo2vo8

---
+++ Syntex/Mo2vo8.js
@@ -0,0 +1,32 @@
+const surveyResponses = [
+  "A",
+  "C",
+  "B",
+  "A",
+  "B",
+  "B",
+  "C",
+  "A",
+  "B",
+  "D",
+  "A",
+  "C",
+  "B",
+  "A",
+];
+
+
+
+const count = surveyResponses.reduce((table,response)=>{
+    // if(table[response]){
+    //     table[response] = table[response] + 1
+    // }else{
+    //     table[response] = 1
+    // }
+
+     table[response] = (table[response] || 0) + 1
+
+    return table
+},{})
+
+console.log(count)
